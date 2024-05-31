fa3c61fe5ef0a725b7d76d8ecedce042fe26ff96
luqin2007 <lq2007lq@hotmail.com>
2024-05-31 14:20:03 +0800

    2024-05-31  14:19 / ea: begin

---
+++ server/ebussiness-admin/src/main/java/com/example/ebussinessadmin/EbussinessAdminApplication.java
@@ -0,0 +1,13 @@
+package com.example.ebussinessadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EbussinessAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EbussinessAdminApplication.class, args);
+    }
+
+}
---
+++ server/ebussiness-admin/src/test/java/com/example/ebussinessadmin/EbussinessAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ebussinessadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EbussinessAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
